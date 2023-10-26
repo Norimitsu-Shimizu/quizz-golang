f45cd3b2b20791beb389463d5dc3e488b0fb45bb
Norimitsu-Shimizu <sparesound31@gmail.com>
2023-10-26 20:18:11 +0900

    feat: ユーザーテーブルのマイグレーション

--- api/main.go
+++ api/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"app/internal/domain"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
+		db := dbInit()
+		db.AutoMigrate(&domain.User{})
 		return c.String(http.StatusOK, "Hello, world!")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
+
+	// db := dbInit()
+	// db.AutoMigrate(&domain.User{})
+}
+
+func dbInit() *gorm.DB {
+	dsn := "user:password@tcp(db:3306)/quizz-golang?charset=utf8mb4&parseTime=true&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
 }
